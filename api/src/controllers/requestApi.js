84b0a764cc9d1d9092888ceb8cc693f958206611
juancdri <juancdri@gmail.com>
2021-11-23 22:05:55 -0300

    controllers

--- api/src/controllers/requestApi.js
+++ api/src/controllers/requestApi.js
@@ -6,32 +6,32 @@ var index = 1;
 var apiKey;
 
 //BUSCAR UNA LISTADO DE RESETAS completas
-const getApiInfo = async ()=>{
+const getApiInfo = async () => {
     switch (index) {
-        case 1:  apiKey = API_KEY_1;break;
-        case 2:  apiKey = API_KEY_2;break;
-        case 3:  apiKey = API_KEY_3;break;
-        case 4:  apiKey = API_KEY_4;break;
-        case 5:  apiKey = API_KEY_5;break;
-        case 6:  apiKey = YOUR_API_KEY;break;
-        default:  apiKey = API_KEY_1;break;
+        case 1: apiKey = API_KEY_1; break;
+        case 2: apiKey = API_KEY_2; break;
+        case 3: apiKey = API_KEY_3; break;
+        case 4: apiKey = API_KEY_4; break;
+        case 5: apiKey = API_KEY_5; break;
+        case 6: apiKey = YOUR_API_KEY; break;
+        default: apiKey = API_KEY_1; break;
     }
-    try{
-        const recipeApi = await axios.get (`https://api.spoonacular.com/recipes/complexSearch?apiKey=${apiKey}&addRecipeInformation=true&number=99`)
+    try {
+        const recipeApi = await axios.get(`https://api.spoonacular.com/recipes/complexSearch?apiKey=${apiKey}&addRecipeInformation=true&number=99`)
         const recipe = recipeApi.data.results;
-        return recipe.map(e=>{
+        return recipe.map(e => {
             return {
                 id: e.id,
                 title: e.title,
-                summary:e.summary,        
+                summary: e.summary,
                 image: e.image,
                 analyzedInstructions: e.analyzedInstructions,
                 score: e.healthScore,
                 diets: e.diets,
-                dishTypes: e.dishTypes,    
+                dishTypes: e.dishTypes,
             }
         })
-    }catch(error){
+    } catch (error) {
         //console.log(error,'error requestApi', index)
         if (index >= 6) {
             index = 1;
@@ -43,6 +43,6 @@ const getApiInfo = async ()=>{
 }
 
 
-module.exports={
+module.exports = {
     getApiInfo
- }
+}
--- api/src/controllers/requestDB.js
+++ api/src/controllers/requestDB.js
@@ -3,20 +3,9 @@ const { Recipe, Type } = require('../db');
 const getBdInfo = async () => {
     try {
         const dbRecetas = await Recipe.findAll({ include: [Type] })
-
-        dbRecetas = recipeAll.map(e => {
-            return {
-                id: e.id,
-                name: e.name,
-                summary: e.summary,
-                score: e.score,
-                instructions: e.instructions,
-                image: e.image,
-                diets: e.diets.map(diet => diet.title)
-            }
-        })
         return dbRecetas
     } catch (error) {
+        console.log(error)
         return [];
     };
 
